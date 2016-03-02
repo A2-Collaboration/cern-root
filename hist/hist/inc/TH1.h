dff06b0c0266095a18ab6726e840f650b7778471
Oliver Steffen <steffen@kph.uni-mainz.de>
2016-03-02 18:32:17 +0100

    TH1: Added menu entry for Scale()

--- hist/hist/inc/TH1.h
+++ hist/hist/inc/TH1.h
@@ -333,7 +333,7 @@ public:
    virtual void     Reset(Option_t *option="");
    virtual void     ResetStats();
    virtual void     SavePrimitive(ostream &out, Option_t *option = "");
-   virtual void     Scale(Double_t c1=1, Option_t *option="");
+   virtual void     Scale(Double_t c1=1, Option_t *option=""); // *MENU*
    virtual void     SetAxisColor(Color_t color=1, Option_t *axis="X");
    virtual void     SetAxisRange(Double_t xmin, Double_t xmax, Option_t *axis="X");
    virtual void     SetBarOffset(Float_t offset=0.25) {fBarOffset = Short_t(1000*offset);}
